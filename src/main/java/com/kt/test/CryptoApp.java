b4a4c24d89e7a9f3a5b201902e2c4e2f6138d6e1
june <loneyjune@yahoo.ca>
2021-07-11 20:50:28 +0900

    encry_decry_2

--- src/main/java/com/kt/test/CryptoApp.java
+++ src/main/java/com/kt/test/CryptoApp.java
@@ -1,7 +1,10 @@
 package com.kt.test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CryptoApp {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Crypto crypto = new BasicCrypto();
 		
 		String data = "Hello world";
@@ -11,5 +14,15 @@ public class CryptoApp {
 		System.out.println("Original : "+data);
 		System.out.println("Encrypted : "+enc);
 		System.out.println("Decrypted: "+dec);
+		
+		//C:\Users\junee\workspace\TestProject
+		byte[] data1 = Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"\\src\\main\\java\\com\\kt\\test\\data1.txt"));
+		byte[] data2 = crypto.encrypt(data1);
+		byte[] data3 = crypto.decrypt(data2);
+		
+		System.out.println("data1"+System.getProperty("user.dir"));
+		Files.write(Paths.get(System.getProperty("user.dir")+"\\src\\main\\java\\com\\kt\\test\\data2.txt"),data2);
+		Files.write(Paths.get(System.getProperty("user.dir")+"\\src\\main\\java\\com\\kt\\test\\data3.txt"),data3);
+		
 	}
 }

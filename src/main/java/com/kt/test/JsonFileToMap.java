bcb25957a0b0cdaae8732ae34c3bc2bf0a365d62
june <loneyjune@yahoo.ca>
2021-07-21 00:17:48 +0900

    jsonFileToMap

---
+++ src/main/java/com/kt/test/JsonFileToMap.java
@@ -0,0 +1,42 @@
+package com.kt.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+public class JsonFileToMap {
+	
+	public static void main(String[] args) { 
+		
+		try { 
+			ObjectMapper mapper = new ObjectMapper(); 
+			
+			Map<String, Object> map = mapper.readValue(new File("c:\\Users\\junee\\user.json"), 
+					new TypeReference<Map<String, Object>>() {}); 
+			
+			System.out.println(map.get("name")); System.out.println(map.get("age")); 
+			System.out.println(map.get("messages")); 
+			
+			ArrayList<String> list = (ArrayList<String>) map.get("messages"); 
+			
+			for (String msg : list) { 
+				
+				System.out.println(msg); 
+			} 
+			} catch (JsonGenerationException e) { 
+				e.printStackTrace(); 
+			} catch (JsonMappingException e) { 
+				e.printStackTrace(); 
+				
+			} catch (IOException e) { 
+				e.printStackTrace(); 
+			}
+	}
+
+}
